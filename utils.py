dc757fa471e6853aaea286daf0c4ad456f4d013b
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-03-13 14:43:01 -0400

    Better string exclusion

--- utils.py
+++ utils.py
@@ -22,9 +22,10 @@ def header(text: str, dockerfile_text: str = None) -> str:
 
 # Modify mamba/conda
 def modify_mamba(mamba_part: list) -> str:
+    not_in_list = ["nbextension","fix-permissions","NB_USER"]
     mamba_part = (
         "\n".join(
-            [x for x in mamba_part if "fix-permissions" not in x and "NB_USER" not in x]
+            [x for x in mamba_part if not any(sub in x for sub in not_in_list)]
         )
         .replace("RUN mamba install --quiet --yes \\", MAMBA_PREFIX)
         .replace("RUN mamba install --quiet --yes", MAMBA_PREFIX + "\n   ")
