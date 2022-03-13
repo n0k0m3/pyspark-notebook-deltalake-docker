f05de4ca724d6cbb41ae6e75da5b2992a72ccd55
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2022-03-13 12:34:20 -0400

    Code formatting with black

--- source_Dockerfile_parts.py
+++ source_Dockerfile_parts.py
@@ -25,12 +25,15 @@ def header(text: str, dockerfile_text: str = None):
 
 # Modify mamba/conda
 def modify_mamba(mamba_part: str) -> str:
-    mamba_part = "\n"\
-        .join([x for x in mamba_part if "fix-permissions" not in x and "NB_USER" not in x])\
-        .replace("RUN mamba install --quiet --yes \\", MAMBA_PREFIX)\
-        .replace("RUN mamba install --quiet --yes", MAMBA_PREFIX+"\n   ")\
-        .replace("mamba clean --all -f -y && \\", "gpuci_mamba_retry clean --all -f -y")\
+    mamba_part = (
+        "\n".join(
+            [x for x in mamba_part if "fix-permissions" not in x and "NB_USER" not in x]
+        )
+        .replace("RUN mamba install --quiet --yes \\", MAMBA_PREFIX)
+        .replace("RUN mamba install --quiet --yes", MAMBA_PREFIX + "\n   ")
+        .replace("mamba clean --all -f -y && \\", "gpuci_mamba_retry clean --all -f -y")
         .strip()
+    )
     return mamba_part
 
 
@@ -40,9 +43,12 @@ def checkout_base_notebook():
     with open("docker-stacks/base-notebook/Dockerfile", "r") as f:
         base_notebook_dockerfile = f.read()
 
-    base_notebook_dockerfile_part += re.findall(
-        r"USER root([\w\W]+)\# Configure environment",
-        base_notebook_dockerfile)[0].strip() + "\n"
+    base_notebook_dockerfile_part += (
+        re.findall(
+            r"USER root([\w\W]+)\# Configure environment", base_notebook_dockerfile
+        )[0].strip()
+        + "\n"
+    )
     with open("src_docker-stacks/Dockerfile.base-notebook", "w") as f:
         f.write(base_notebook_dockerfile_part)
 
@@ -53,9 +59,12 @@ def checkout_minimal_notebook():
     with open("docker-stacks/minimal-notebook/Dockerfile", "r") as f:
         minimal_notebook_dockerfile = f.read()
 
-    minimal_notebook_dockerfile_part += re.findall(
-        r"USER root([\w\W]+)\# Switch back to jovyan",
-        minimal_notebook_dockerfile)[0].strip() + "\n"
+    minimal_notebook_dockerfile_part += (
+        re.findall(
+            r"USER root([\w\W]+)\# Switch back to jovyan", minimal_notebook_dockerfile
+        )[0].strip()
+        + "\n"
+    )
     with open("src_docker-stacks/Dockerfile.minimal-notebook", "w") as f:
         f.write(minimal_notebook_dockerfile_part)
 
@@ -66,14 +75,24 @@ def checkout_scipy_notebook():
     with open("docker-stacks/scipy-notebook/Dockerfile", "r") as f:
         scipy_notebook_dockerfile = f.read()
 
-    apt_part = re.findall(
-        r"USER root([\w\W]+)USER \${NB_UID}\n\n# Install Python 3 packages",
-        scipy_notebook_dockerfile)[0].strip() + "\n"
+    apt_part = (
+        re.findall(
+            r"USER root([\w\W]+)USER \${NB_UID}\n\n# Install Python 3 packages",
+            scipy_notebook_dockerfile,
+        )[0].strip()
+        + "\n"
+    )
 
-    mamba_part = re.findall(r"USER \${NB_UID}([\w\W]+)# Import matplotlib",
-                            scipy_notebook_dockerfile)[0].strip().split("\n")
-    mamba_part = modify_mamba(mamba_part)\
-        .replace("rm -rf /tmp/facets && \\", "rm -rf /tmp/facets")
+    mamba_part = (
+        re.findall(
+            r"USER \${NB_UID}([\w\W]+)# Import matplotlib", scipy_notebook_dockerfile
+        )[0]
+        .strip()
+        .split("\n")
+    )
+    mamba_part = modify_mamba(mamba_part).replace(
+        "rm -rf /tmp/facets && \\", "rm -rf /tmp/facets"
+    )
 
     scipy_notebook_dockerfile_part += apt_part + "\n" + mamba_part + "\n"
     with open("src_docker-stacks/Dockerfile.scipy-notebook", "w") as f:
@@ -85,38 +104,66 @@ def checkout_pyspark_notebook():
     with open("docker-stacks/pyspark-notebook/Dockerfile", "r") as f:
         pyspark_notebook_dockerfile = f.read()
 
-    fix_part = re.findall(r"(\# Fix DL4006[\w\W]+)USER root",
-                          pyspark_notebook_dockerfile)[0].strip() + "\n"
-    spark_part = re.findall(r"(\# Spark dependencies[\w\W]+)USER \${NB_UID}", pyspark_notebook_dockerfile)[0]\
-                    .replace("RUN fix-permissions \"/etc/ipython/\"","").strip() + "\n"
-    mamba_part = re.findall(r"USER \${NB_UID}([\w\W]+)WORKDIR \"\${HOME}\"",
-                            pyspark_notebook_dockerfile)[0].strip().split("\n")
+    fix_part = (
+        re.findall(r"(\# Fix DL4006[\w\W]+)USER root", pyspark_notebook_dockerfile)[
+            0
+        ].strip()
+        + "\n"
+    )
+    spark_part = (
+        re.findall(
+            r"(\# Spark dependencies[\w\W]+)USER \${NB_UID}",
+            pyspark_notebook_dockerfile,
+        )[0]
+        .replace('RUN fix-permissions "/etc/ipython/"', "")
+        .strip()
+        + "\n"
+    )
+    mamba_part = (
+        re.findall(
+            r"USER \${NB_UID}([\w\W]+)WORKDIR \"\${HOME}\"", pyspark_notebook_dockerfile
+        )[0]
+        .strip()
+        .split("\n")
+    )
 
     mamba_part = modify_mamba(mamba_part)
 
-    pyspark_notebook_dockerfile_part += fix_part + "\n" + spark_part + "\n" + mamba_part + "\n"
+    pyspark_notebook_dockerfile_part += (
+        fix_part + "\n" + spark_part + "\n" + mamba_part + "\n"
+    )
     with open("src_docker-stacks/Dockerfile.pyspark-notebook", "w") as f:
         f.write(pyspark_notebook_dockerfile_part)
 
-    shutil.copyfile("docker-stacks/pyspark-notebook/ipython_kernel_config.py",
-                    ".build/ipython_kernel_config.py")
+    shutil.copyfile(
+        "docker-stacks/pyspark-notebook/ipython_kernel_config.py",
+        ".build/ipython_kernel_config.py",
+    )
 
 
 def checkout_all_spark_notebook():
-    all_spark_notebook_dockerfile_part = header(
-        "From: jupyter/all-spark-notebook")
+    all_spark_notebook_dockerfile_part = header("From: jupyter/all-spark-notebook")
     with open("docker-stacks/all-spark-notebook/Dockerfile", "r") as f:
         all_spark_notebook_dockerfile = f.read()
 
-    apt_part = modify_mamba(
-        re.findall(
-            r"USER root([\w\W]+)USER \${NB_UID}",
-            all_spark_notebook_dockerfile)[0].strip().split("\n")) + "\n"
-    mamba_part = re.findall(
-        r"USER \${NB_UID}([\w\W]+)",
-        all_spark_notebook_dockerfile)[0].strip().split("\n")
-    mamba_part = modify_mamba(mamba_part)\
-                    .replace("install --sys-prefix && \\", "install --sys-prefix")
+    apt_part = (
+        modify_mamba(
+            re.findall(
+                r"USER root([\w\W]+)USER \${NB_UID}", all_spark_notebook_dockerfile
+            )[0]
+            .strip()
+            .split("\n")
+        )
+        + "\n"
+    )
+    mamba_part = (
+        re.findall(r"USER \${NB_UID}([\w\W]+)", all_spark_notebook_dockerfile)[0]
+        .strip()
+        .split("\n")
+    )
+    mamba_part = modify_mamba(mamba_part).replace(
+        "install --sys-prefix && \\", "install --sys-prefix"
+    )
 
     all_spark_notebook_dockerfile_part += apt_part + "\n" + mamba_part + "\n"
     with open("src_docker-stacks/Dockerfile.all-spark-notebook", "w") as f:
@@ -128,12 +175,20 @@ def checkout_r_notebook():
     with open("docker-stacks/r-notebook/Dockerfile", "r") as f:
         r_notebook_dockerfile = f.read()
 
-    apt_part = re.findall(r"USER root([\w\W]+)USER \${NB_UID}",
-                          r_notebook_dockerfile)[0].strip() + "\n"
-    mamba_part = re.findall(r"USER \${NB_UID}([\w\W]+)",
-                            r_notebook_dockerfile)[0].strip().split("\n")
-    mamba_part = modify_mamba("\n".join(mamba_part))\
-                    .replace("install --sys-prefix && \\", "install --sys-prefix")
+    apt_part = (
+        re.findall(r"USER root([\w\W]+)USER \${NB_UID}", r_notebook_dockerfile)[
+            0
+        ].strip()
+        + "\n"
+    )
+    mamba_part = (
+        re.findall(r"USER \${NB_UID}([\w\W]+)", r_notebook_dockerfile)[0]
+        .strip()
+        .split("\n")
+    )
+    mamba_part = modify_mamba("\n".join(mamba_part)).replace(
+        "install --sys-prefix && \\", "install --sys-prefix"
+    )
 
     r_notebook_dockerfile_part += apt_part + "\n" + mamba_part + "\n"
     with open("src_docker-stacks/Dockerfile.r-notebook", "w") as f:
